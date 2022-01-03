b10365acc69671038fd424cc91b4ddf2ecaafeb1
Nisha Dhiman <91591950+nishaslidetech@users.noreply.github.com>
2022-01-03 20:09:08 +0530

    Update SLI_search_facebook_paid_login.java

--- src/test/java/StepDefination/SLI_search_facebook_paid_login.java
+++ src/test/java/StepDefination/SLI_search_facebook_paid_login.java
@@ -60,13 +60,13 @@ public class SLI_search_facebook_paid_login extends BaseClass {
 			if (!driver.findElements(By.xpath("//input[@value='Log In']")).isEmpty()) {
 				driver.findElement(By.xpath("//input[@value='Log In']")).click();
 			}
-				
+				else {
 			WebElement fb_login = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='loginbutton']")));
 			
 			
 			fb_login.click();
 			Thread.sleep(3000);
-		
+				}
 		} catch (NoSuchElementException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
