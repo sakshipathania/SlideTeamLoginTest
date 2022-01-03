df7f8b1ea36349aaa631897bb6fc907b85861382
Nisha Dhiman <91591950+nishaslidetech@users.noreply.github.com>
2022-01-03 17:04:39 +0530

    Update Facebook_free_login.java

--- src/test/java/StepDefination/Facebook_free_login.java
+++ src/test/java/StepDefination/Facebook_free_login.java
@@ -99,7 +99,7 @@ public class Facebook_free_login extends BaseClass {
 		try {
 			//WebElement fb_login = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='loginbutton']")));
 			WebElement fb_login_btn2 = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Log In")));
-			fb_login.click();
+			fb_login_btn2.click();
 
 		} catch (NoSuchElementException e) {
 
