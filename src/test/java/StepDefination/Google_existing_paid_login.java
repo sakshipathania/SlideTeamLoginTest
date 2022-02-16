ccdf5f1e73f0152525b838712d527405c8644133
Nisha Dhiman <91591950+nishaslidetech@users.noreply.github.com>
2022-02-16 12:55:03 +0530

    Update Google_existing_paid_login.java

--- src/test/java/StepDefination/Google_existing_paid_login.java
+++ src/test/java/StepDefination/Google_existing_paid_login.java
@@ -87,6 +87,12 @@ public class Google_existing_paid_login extends BaseClass {
 			WebElement next_2 = driver.findElement(By.cssSelector("#passwordNext > div > button > span"));
 
 			next_2.click();
+			Thread.sleep(2000);
+			if (!driver.findElements(By.xpath("//div[@class='login-attempt-popup']")).isEmpty()) {
+				WebElement approve = wait
+						.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='confirm-approve']")));
+				approve.click();
+			}
 			  Thread.sleep(9000);
 			//Thread.sleep(9000);
 			
